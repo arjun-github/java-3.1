ab1631b91f95d85063d95cbf91d0375791c94a01
arjun-github <arjunsingh.gcet@gmail.com>
2017-04-22 15:37:25 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+static{      
+print(10);
+}
+static void print (int x) {
+System.out.println (x);
+System.exit(0);
+}
+}
+
+
+// b. Run Time Error, no main method
+// If we put the main method then output will be c ie.10.
